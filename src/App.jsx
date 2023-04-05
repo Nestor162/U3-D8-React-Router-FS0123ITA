a5dc6e90cfded111c7a672ac191775820a05b547
Nestor162 <nestorcicardini@gmail.com>
2023-04-05 14:51:48 +0200

    Struttura iniziale Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NetflixNav from "./components/NetflixNav";
 import MainContent from "./components/MainContent";
 import NetflixFooter from "./components/NetflixFooter";
-/* import AccountPage from "./components/AccountPage"; */
-/* import SettingsPage from "./components/settingsPage"; */
+import AccountPage from "./components/AccountPage";
+import SettingsPage from "./components/settingsPage";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
     <main className="App">
-      <NetflixNav />
-      <MainContent />
-      {/*   <SettingsPage /> */}
-      {/*       <AccountPage /> */}
-      <NetflixFooter />
+      <BrowserRouter>
+        <NetflixNav />
+
+        <Routes>
+          <Route path="/" element={<MainContent />} />
+          <Route path="/settings" element={<SettingsPage />} />
+          <Route path="/account" element={<AccountPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+
+        <NetflixFooter />
+      </BrowserRouter>
     </main>
   );
 }

01da9bcd648b59d06150768a723162bed10b79fc
Nestor162 <nestorcicardini@gmail.com>
2023-04-05 15:24:40 +0200

    Collegamenti navbar

--- src/components/NetflixNav.jsx
+++ src/components/NetflixNav.jsx
@@ -3,61 +3,89 @@ import logo from "../img/netflix_logo.png";
 import avatar from "../img/avatar.png";
 import kids from "../img/kids_icon.png";
 import { Search, Bell } from "react-bootstrap-icons";
+import { Link, useLocation } from "react-router-dom";
 
-const NetflixNav = () => (
-  <Navbar expand="lg" variant="dark" className="px-4">
-    <Container fluid>
-      <Navbar.Brand href="#">
-        <img src={logo} alt="Netflix Logo" width="148px" />
-      </Navbar.Brand>
+const NetflixNav = () => {
+  const location = useLocation();
+  return (
+    <Navbar expand="lg" variant="dark" className="px-4">
+      <Container fluid>
+        <Navbar.Brand href="#">
+          <img src={logo} alt="Netflix Logo" width="148px" />
+        </Navbar.Brand>
 
-      <Navbar.Collapse id="mainNav">
-        <Nav className="me-auto mb-2 mb-lg-0">
-          <Nav.Link href="#">Home</Nav.Link>
-          <Nav.Link href="#" active>
-            TV Shows
-          </Nav.Link>
-          <Nav.Link href="#">Movies</Nav.Link>
-          <Nav.Link href="#">Recently Added</Nav.Link>
-          <Nav.Link href="#">My List</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-      <div id="right-icons" className="d-flex gap-3 align-items-center text-light">
-        <Search size={20}></Search>
-        <span>KIDS</span>
-        <Bell size={20}></Bell>
-        <div className="dropdown">
-          <NavDropdown title={<img src={avatar} alt="User Avatar" width="48px" />} id="basic-nav-dropdown">
-            <NavDropdown.Item className="text-light" href="#">
-              <img src={kids} alt="User Avatar" width="48px" /> Kids
-            </NavDropdown.Item>
-            <NavDropdown.Item className="text-light" href="#">
-              Manage Profiles
-            </NavDropdown.Item>
+        <Navbar.Collapse id="mainNav">
+          <Nav className="me-auto mb-2 mb-lg-0">
+            <Link href="#" className={`nav-link ${location.pathname === "/" ? "active fw-bold" : ""}`} to="/">
+              Home
+            </Link>
+            <Link
+              href="#"
+              className={`nav-link ${location.pathname === "/tv-shows" ? "active fw-bold" : ""}`}
+              to="/tv-shows"
+            >
+              TV Shows
+            </Link>
+            <Link
+              href="#"
+              className={`nav-link ${location.pathname === "/movies" ? "active fw-bold" : ""}`}
+              to="/movies"
+            >
+              Movies
+            </Link>
+            <Link
+              href="#"
+              className={`nav-link ${location.pathname === "/recently-added" ? "active fw-bold" : ""}`}
+              to="/recently-added"
+            >
+              Recently Added
+            </Link>
+            <Link
+              href="#"
+              className={`nav-link ${location.pathname === "/my-list" ? "active fw-bold" : ""}`}
+              to="/my-list"
+            >
+              My List
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
+        <div id="right-icons" className="d-flex gap-3 align-items-center text-light">
+          <Search size={20}></Search>
+          <span>KIDS</span>
+          <Bell size={20}></Bell>
+          <div className="dropdown">
+            <NavDropdown title={<img src={avatar} alt="User Avatar" width="48px" />} id="basic-nav-dropdown">
+              <NavDropdown.Item className="text-light" href="#">
+                <img src={kids} alt="User Avatar" width="48px" /> Kids
+              </NavDropdown.Item>
+              <NavDropdown.Item className="text-light" href="#">
+                Manage Profiles
+              </NavDropdown.Item>
 
-            <hr className="dropdown-divider" />
+              <hr className="dropdown-divider" />
 
-            <NavDropdown.Item className="text-light" href="#">
-              Edit Profile
-            </NavDropdown.Item>
-            <NavDropdown.Item className="text-light" href="#">
-              Account
-            </NavDropdown.Item>
-            <NavDropdown.Item className="text-light" href="#">
-              Help Center
-            </NavDropdown.Item>
+              <NavDropdown.Item className="text-light" href="#">
+                Edit Profile
+              </NavDropdown.Item>
+              <NavDropdown.Item className="text-light" href="#">
+                Account
+              </NavDropdown.Item>
+              <NavDropdown.Item className="text-light" href="#">
+                Help Center
+              </NavDropdown.Item>
 
-            <hr className="dropdown-divider" />
+              <hr className="dropdown-divider" />
 
-            <NavDropdown.Item className="text-light" href="#">
-              Sign out of NETFLIX
-            </NavDropdown.Item>
-          </NavDropdown>
+              <NavDropdown.Item className="text-light" href="#">
+                Sign out of NETFLIX
+              </NavDropdown.Item>
+            </NavDropdown>
+          </div>
+
+          <Navbar.Toggle />
         </div>
-
-        <Navbar.Toggle />
-      </div>
-    </Container>
-  </Navbar>
-);
+      </Container>
+    </Navbar>
+  );
+};
 export default NetflixNav;
